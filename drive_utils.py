3e55af0fdc25cde0105c18a73a41dcec45ed6e06
MartinaMirek <betynka@betynka.cz>
2025-05-27 22:51:38 +0200

    Update drive_utils.py

--- drive_utils.py
+++ drive_utils.py
@@ -4,14 +4,12 @@ from google.oauth2 import service_account
 from googleapiclient.discovery import build
 
 def nacist_soubory_z_disku(slozka_id):
-    service_account_info = json.loads(os.environ["GOOGLE_SERVICE_ACCOUNT"])
-    creds = service_account.Credentials.from_service_account_info(service_account_info)
+    info = json.loads(os.environ["GOOGLE_SERVICE_ACCOUNT"])
+    creds = service_account.Credentials.from_service_account_info(info)
     service = build("drive", "v3", credentials=creds)
-
     results = service.files().list(
         q=f"'{slozka_id}' in parents and trashed = false",
         pageSize=100,
         fields="files(id, name)"
     ).execute()
-
     return results.get("files", [])
