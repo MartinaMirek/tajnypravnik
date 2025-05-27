a9cc5cb317d8be726d5ad618c9e9f365c48a797b
MartinaMirek <betynka@betynka.cz>
2025-05-27 22:40:29 +0200

    Update drive_utils.py

--- drive_utils.py
+++ drive_utils.py
@@ -1,24 +1,17 @@
-
 import os
 import json
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
+from google.oauth2 import service_account
 from googleapiclient.discovery import build
 
-SCOPES = ["https://www.googleapis.com/auth/drive.metadata.readonly"]
-credentials_path = "credentials.json"
-
-def authenticate():
-    flow = InstalledAppFlow.from_client_secrets_file(credentials_path, SCOPES)
-    creds = flow.run_local_server(port=0)
-    return creds
-
 def nacist_soubory_z_disku(slozka_id):
-    creds = authenticate()
+    service_account_info = json.loads(os.environ["GOOGLE_SERVICE_ACCOUNT"])
+    creds = service_account.Credentials.from_service_account_info(service_account_info)
     service = build("drive", "v3", credentials=creds)
+
     results = service.files().list(
         q=f"'{slozka_id}' in parents and trashed = false",
         pageSize=100,
         fields="files(id, name)"
     ).execute()
+
     return results.get("files", [])
